d366f6644731ed8ecf9ac590b359eed66e72e465
DevExpressExampleBot <fakeEmail>
2022-12-18 19:44:16 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,64 +1,52 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Globalization
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace DXPivotGrid_FindCells
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			AddHandler pivotGrid.CustomFieldValueCells, AddressOf pivotGrid_CustomFieldValueCells
-		End Sub
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			PivotHelper.FillPivot(pivotGrid)
-			pivotGrid.DataSource = PivotHelper.GetDataTable()
-			pivotGrid.BestFit()
-		End Sub
 
-		' Handles the CustomFieldValueCells event to remove columns with
-		' zero summary values.
-        Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, _
-                                                    ByVal e As PivotCustomFieldValueCellsEventArgs)
-            If pivotGrid.DataSource Is Nothing Then
-                Return
-            End If
-            If rbDefault.IsChecked = True Then
-                Return
-            End If
+    Public Partial Class Window1
+        Inherits Window
 
+        Public Sub New()
+            Me.InitializeComponent()
+            AddHandler Me.pivotGrid.CustomFieldValueCells, New PivotCustomFieldValueCellsEventHandler(AddressOf pivotGrid_CustomFieldValueCells)
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            PivotHelper.FillPivot(Me.pivotGrid)
+            Me.pivotGrid.DataSource = GetDataTable()
+            Me.pivotGrid.BestFit()
+        End Sub
+
+        ' Handles the CustomFieldValueCells event to remove columns with
+        ' zero summary values.
+        Private Sub pivotGrid_CustomFieldValueCells(ByVal sender As Object, ByVal e As PivotCustomFieldValueCellsEventArgs)
+            If Me.pivotGrid.DataSource Is Nothing Then Return
+            If Me.rbDefault.IsChecked = True Then Return
             ' Obtains the first encountered column header whose column
             ' matches the specified condition, represented by a predicate.
-            Dim cell As FieldValueCell = _
-                e.FindCell(True, New Predicate(Of Object())( _
-                           Function(dataCellValues) AnonymousMethod1(dataCellValues)))
+            ' Defines the predicate returning true for columns
+            ' that contain only zero summary values.
+            Dim cell As FieldValueCell = e.FindCell(True, New Predicate(Of Object())(Function(ByVal dataCellValues)
+                For Each value As Object In dataCellValues
+                    If Not Equals(CDec(0), value) Then Return False
+                Next
 
+                Return True
+            End Function))
             ' If any column header matches the condition, this column is removed.
-            If cell IsNot Nothing Then
-                e.Remove(cell)
+            If cell IsNot Nothing Then e.Remove(cell)
+        End Sub
+
+        Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, ByVal e As PivotFieldDisplayTextEventArgs)
+            If e.Field Is Me.pivotGrid.Fields(Month) Then
+                e.DisplayText = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(e.Value))
             End If
         End Sub
-		
-        ' Defines the predicate returning true for columns
-        ' that contain only zero summary values.
-        Private Function AnonymousMethod1(ByVal dataCellValues() As Object) As Boolean
-            For Each value As Object In dataCellValues
-                If (Not Object.Equals(CDec(0), value)) Then
-                    Return False
-                End If
-            Next value
-            Return True
-        End Function
-        Private Sub pivotGrid_FieldValueDisplayText(ByVal sender As Object, _
-                                                    ByVal e As PivotFieldDisplayTextEventArgs)
-            If Object.Equals(e.Field, pivotGrid.Fields(PivotHelper.Month)) Then
-                e.DisplayText = _
-                    CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(CInt(Fix(e.Value)))
-            End If
+
+        Private Sub rbDefault_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGrid.LayoutChanged()
         End Sub
-		Private Sub rbDefault_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGrid.LayoutChanged()
-		End Sub
-	End Class
+    End Class
 End Namespace
